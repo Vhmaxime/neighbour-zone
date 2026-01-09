e114e347cef7cbc3df4e9981f01d41c38076d362
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-09 18:52:45 +0100

    feat(routes): add route guards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,12 +7,12 @@ import { Settings } from './pages/settings/settings';
 import { Friends } from './pages/friends/friends';
 import { authGuard } from './guards/auth-guard';
 import { guestGuard } from './guards/guest-guard';
-import { Home } from './pages/home/home'
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-  { path: 'home', component: Home },
   { path: 'login', component: Login, canActivate: [guestGuard] },
   { path: 'register', component: Register, canActivate: [guestGuard] },
+  { path: 'home', component: Home, canActivate: [guestGuard] },
   {
     path: 'dashboard',
     component: Dashboard,
@@ -21,5 +21,4 @@ export const routes: Routes = [
   { path: 'profile', component: ProfilePage, canActivate: [authGuard] },
   { path: 'settings', component: Settings, canActivate: [authGuard] },
   { path: 'friends', component: Friends, canActivate: [authGuard] },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
