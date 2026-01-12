96e9fbddfe7c39b2032262f24988b3d0fed1a727
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-12 15:34:48 +0100

    feat(friend): implement friend request handling endpoints

--- backend/src/routes/friend.ts
+++ backend/src/routes/friend.ts
@@ -2,6 +2,11 @@ import { Hono } from "hono";
 import authMiddleware from "../middleware/auth.js";
 import { Variables } from "../types/index.js";
 import { db } from "../database/index.js";
+import { zValidator } from "@hono/zod-validator";
+import { idSchema } from "../schemas/index.js";
+import { friendshipsTable } from "../database/schema.js";
+import { cp } from "node:fs";
+import { eq } from "drizzle-orm";
 
 const friendRouter = new Hono<{ Variables: Variables }>();
 
@@ -50,27 +55,185 @@ friendRouter.get("/list", async (c) => {
 });
 
 friendRouter.get("/requests ", async (c) => {
-  return c.json({});
+  const { sub: userId } = c.get("jwtPayload");
+
+  const requests = await db.query.friendshipsTable.findMany({
+    where: {
+      AND: [{ userId2: { eq: userId } }, { status: { eq: "pending" } }],
+    },
+    columns: {},
+    with: {
+      user1: {
+        columns: {
+          id: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+  const requestList = requests.map((friendship) => friendship.user1);
+  return c.json({ requests: requestList });
 });
 
 friendRouter.get("/sent ", async (c) => {
-  return c.json({});
+  const { sub: userId } = c.get("jwtPayload");
+  const sentRequests = await db.query.friendshipsTable.findMany({
+    where: {
+      AND: [{ userId1: { eq: userId } }, { status: { eq: "pending" } }],
+    },
+    columns: {},
+    with: {
+      user2: {
+        columns: {
+          id: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+  const sentList = sentRequests.map((friendship) => friendship.user2);
+  return c.json({ sent: sentList });
 });
 
-friendRouter.post("/request/:id ", async (c) => {
-  return c.json({});
-});
+friendRouter.post(
+  "/request/:id",
+  zValidator("param", idSchema, (result, c) => {
+    if (!result.success) {
+      console.log(result.error);
+      return c.json({ message: "Bad Request" }, 400);
+    }
+  }),
+  async (c) => {
+    const { sub: userId } = c.get("jwtPayload");
+    const { id: friendId } = c.req.valid("param");
 
-friendRouter.patch("/accept/:id ", async (c) => {
-  return c.json({});
-});
+    const existingFriendship = await db.query.friendshipsTable.findFirst({
+      where: {
+        OR: [
+          { AND: [{ userId1: { eq: userId } }, { userId2: { eq: friendId } }] },
+          { AND: [{ userId1: { eq: friendId } }, { userId2: { eq: userId } }] },
+        ],
+      },
+    });
 
-friendRouter.delete("/reject/:id ", async (c) => {
-  return c.json({});
-});
+    if (existingFriendship && existingFriendship.status === "pending") {
+      return c.json({ message: "Friend request already pending" }, 400);
+    }
 
-friendRouter.delete("/remove/:id ", async (c) => {
-  return c.json({});
-});
+    if (existingFriendship && existingFriendship.status === "accepted") {
+      return c.json({ message: "You are already friends" }, 400);
+    }
+
+    await db.insert(friendshipsTable).values({
+      userId1: userId,
+      userId2: friendId,
+      status: "pending",
+    });
+
+    return c.json({ message: "ok" }, 200);
+  }
+);
+
+friendRouter.patch(
+  "/accept/:id ",
+  zValidator("param", idSchema, (result, c) => {
+    if (!result.success) {
+      console.log(result.error);
+      return c.json({ message: "Bad Request" }, 400);
+    }
+  }),
+  async (c) => {
+    const { sub: userId } = c.get("jwtPayload");
+    const { id: friendId } = c.req.valid("param");
+
+    const friendship = await db.query.friendshipsTable.findFirst({
+      where: {
+        AND: [
+          { userId1: { eq: friendId } },
+          { userId2: { eq: userId } },
+          { status: { eq: "pending" } },
+        ],
+      },
+    });
+
+    if (!friendship) {
+      return c.json({ message: "No pending friend request found" }, 404);
+    }
+    await db
+      .update(friendshipsTable)
+      .set({ status: "accepted" })
+      .where(eq(friendshipsTable.id, friendship.id));
+
+    return c.json({ message: "ok" }, 200);
+  }
+);
+
+friendRouter.delete(
+  "/reject/:id ",
+  zValidator("param", idSchema, (result, c) => {
+    if (!result.success) {
+      console.log(result.error);
+      return c.json({ message: "Bad Request" }, 400);
+    }
+  }),
+  async (c) => {
+    const { sub: userId } = c.get("jwtPayload");
+    const { id: friendId } = c.req.valid("param");
+
+    const friendship = await db.query.friendshipsTable.findFirst({
+      where: {
+        AND: [
+          { userId1: { eq: friendId } },
+          { userId2: { eq: userId } },
+          { status: { eq: "pending" } },
+        ],
+      },
+    });
+
+    if (!friendship) {
+      return c.json({ message: "No pending friend request found" }, 404);
+    }
+
+    await db
+      .delete(friendshipsTable)
+      .where(eq(friendshipsTable.id, friendship.id));
+
+    return c.json({ message: "ok" }, 200);
+  }
+);
+
+friendRouter.delete(
+  "/remove/:id ",
+  zValidator("param", idSchema, (result, c) => {
+    if (!result.success) {
+      console.log(result.error);
+      return c.json({ message: "Bad Request" }, 400);
+    }
+  }),
+  async (c) => {
+    const { sub: userId } = c.get("jwtPayload");
+    const { id: friendId } = c.req.valid("param");
+
+    const friendship = await db.query.friendshipsTable.findFirst({
+      where: {
+        OR: [
+          { AND: [{ userId1: { eq: userId } }, { userId2: { eq: friendId } }] },
+          { AND: [{ userId1: { eq: friendId } }, { userId2: { eq: userId } }] },
+        ],
+      },
+    });
+    if (!friendship) {
+      return c.json({ message: "Not found" }, 404);
+    }
+
+    await db
+      .delete(friendshipsTable)
+      .where(eq(friendshipsTable.id, friendship.id));
+
+    return c.json({ message: "ok" }, 200);
+  }
+);
 
 export default friendRouter;
