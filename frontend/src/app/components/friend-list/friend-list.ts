6ae86fb2c7f330434a6f2950652e722c851bd5ce
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-12 16:18:54 +0100

    feat(friend-list): add friend list component files

---
+++ frontend/src/app/components/friend-list/friend-list.ts
@@ -0,0 +1,9 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-list',
+  imports: [],
+  templateUrl: './friend-list.html',
+  styleUrl: './friend-list.css',
+})
+export class FriendList {}
