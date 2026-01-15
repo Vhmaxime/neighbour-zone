d65a4120b6c7bbc94992aea1d2a0b5d5c4363547
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-15 13:11:23 +0100

    refactor(marketplace-details): improve loading logic

--- frontend/src/app/pages/marketplace-details/marketplace-details.ts
+++ frontend/src/app/pages/marketplace-details/marketplace-details.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { Observable, switchMap, catchError, of, tap, map } from 'rxjs';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 import { MarketplaceItem } from '../../types/api.types';
 import { Title } from '@angular/platform-browser';
 import { MarketplaceService } from '../../services/marketplace';
@@ -9,37 +9,40 @@ import { MarketplaceService } from '../../services/marketplace';
 @Component({
   selector: 'app-marketplace-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './marketplace-details.html',
   styleUrl: './marketplace-details.css',
 })
 export class MarketplaceDetails {
-  item$: Observable<MarketplaceItem | null>;
-
-  private route = inject(ActivatedRoute);
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
   private marketplaceService = inject(MarketplaceService);
   private titleService = inject(Title);
+  private itemId = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  public isLoading = signal(false);
+  public item = signal<MarketplaceItem | null>(null);
 
-  constructor() {
-    this.item$ = this.route.paramMap.pipe(
-      switchMap((params) => {
-        const id = params.get('id');
-        if (!id) return of(null);
+  public ngOnInit() {
+    this.loadItem();
+  }
 
-        return this.marketplaceService
-          .getMarketplaceItem(id)
-          .pipe(map((response: any) => (response.marketplace ? response.marketplace : response)));
-      }),
-      tap((data) => {
-        if (data) {
-          // Set dynamic title with Price
-          this.titleService.setTitle(`${data.title} - ${data.price}€ | Neighbour Zone`);
-        }
-      }),
-      catchError((error) => {
-        console.error('Error loading item:', error);
-        return of(null);
+  private loadItem() {
+    this.isLoading.set(true);
+    firstValueFrom(this.marketplaceService.getMarketplaceItem(this.itemId))
+      .then((response: any) => {
+        const item = response.marketplace ? response.marketplace : response;
+        this.item.set(item);
+        console.log(item);
+        this.titleService.setTitle(`${item.title} - ${item.price}€ | Neighbour Zone`);
       })
-    );
+      .catch((error) => {
+        if (error.status === 404) {
+          this.router.navigate(['/not-found']);
+        }
+        console.error('Error loading item:', error);
+      })
+      .finally(() => {
+        this.isLoading.set(false);
+      });
   }
 }
