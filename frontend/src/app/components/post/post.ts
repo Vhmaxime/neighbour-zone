4b4772357368956eee9857ba49c5521fd4d89885
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-15 19:34:30 +0100

    refactor(post): remove unused service and like logic

--- frontend/src/app/components/post/post.ts
+++ frontend/src/app/components/post/post.ts
@@ -2,8 +2,6 @@ import { Component, inject, input } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Post as P } from '../../types/api.types';
-import { PostService } from '../../services/post';
-import { firstValueFrom } from 'rxjs';
 import { LikeButton } from '../like-button/like-button';
 
 @Component({
@@ -13,27 +11,5 @@ import { LikeButton } from '../like-button/like-button';
   styleUrl: './post.css',
 })
 export class Post {
-  private postService = inject(PostService);
   public post = input.required<P>();
-
-  public toggleLike() {
-    const liked = this.post().liked;
-
-    this.post().liked = !liked;
-
-    if (liked) {
-      this.post().likes -= 1;
-    } else {
-      this.post().likes += 1;
-    }
-
-    firstValueFrom(this.postService.likePost(this.post().id)).catch(() => {
-      this.post().liked = liked;
-      if (liked) {
-        this.post().likes += 1;
-      } else {
-        this.post().likes -= 1;
-      }
-    });
-  }
 }
