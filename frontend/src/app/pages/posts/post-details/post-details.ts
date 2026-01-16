8e4e7a4cbb1f3434e047de3178d698fae8788354
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-16 12:48:47 +0100

    refactor(post-details): improve post details component

--- frontend/src/app/pages/posts/post-details/post-details.ts
+++ frontend/src/app/pages/posts/post-details/post-details.ts
@@ -1,6 +1,6 @@
-import { Component, inject, input, signal, computed, effect } from '@angular/core';
-import { CommonModule, DatePipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal, computed } from '@angular/core';
+import { DatePipe, Location } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { PostService } from '../../../services/post';
 import { Post } from '../../../types/api.types';
 import { firstValueFrom } from 'rxjs';
@@ -8,47 +8,48 @@ import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'app-post-details',
-  standalone: true,
-  imports: [CommonModule, RouterLink, DatePipe],
+  imports: [RouterLink, DatePipe],
   templateUrl: './post-details.html',
   styleUrl: './post-details.css',
 })
 export class PostDetails {
-  // This automatically captures the ':id' from the route
-  id = input.required<string>();
-
+  private activatedRoute = inject(ActivatedRoute);
   private postService = inject(PostService);
+  private router = inject(Router);
+  private location = inject(Location);
   private authService = inject(AuthService);
-  
+  private postId = this.activatedRoute.snapshot.paramMap.get('id') as string;
   public post = signal<Post | null>(null);
-  public isLoading = signal(true);
+  public isLoading = signal(false);
+  public isError = signal(false);
+  public isAuthor = computed<boolean>(
+    () => this.post()?.author.id === this.authService.getUser()?.sub
+  );
 
+  public ngOnInit() {
+    this.loadPost();
+  }
 
-  public isAuthor = computed(() => {
-    const currentPost = this.post();
-    const currentUser = this.authService.getUser() as any;
+  private loadPost() {
+    this.isLoading.set(true);
+    firstValueFrom(this.postService.getPost(this.postId))
+      .then((data) => {
+        this.post.set(data.post);
+      })
+      .catch((error) => {
+        if (error.status === 404) {
+          this.router.navigate(['/not-found']);
+        } else {
+          console.error('Error loading user content:', error);
+          this.isError.set(true);
+        }
+      })
+      .finally(() => {
+        this.isLoading.set(false);
+      });
+  }
 
-    if (!currentPost || !currentUser) return false;
-
-    return currentPost.author.id === currentUser.id;
-  });
-
-  constructor() {
-  effect(async () => {
-    const postId = this.id();
-    if (!postId) return;
-
-    try {
-      this.isLoading.set(true);
-      const response = await firstValueFrom(this.postService.getPost(postId));
-      this.post.set(response?.post || response);
-    } catch (error) {
-      console.error('API failed', error);
-      this.post.set(null); 
-    } finally {
-      // Ensures the spinner disappears no matter what
-      this.isLoading.set(false); 
-    }
-  });
+  public goBack() {
+    this.location.back();
   }
 }
