9e53a70b0d9da7709da5dac5c970e784befd888e
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-17 19:56:56 +0100

    feat(create-event): enhance form validation

--- frontend/src/app/pages/events/create-event/create-event.ts
+++ frontend/src/app/pages/events/create-event/create-event.ts
@@ -1,64 +1,87 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { EventService } from '../../../services/event';
 import { CreateEventRequest } from '../../../types/api.types';
-import { LocationSearchBar } from '../../../components/location-search-bar/location-search-bar';
+import { LocationSearchResults } from '../../../components/location-search-bar/location-search-results';
 import { NominatimLocation } from '../../../types/nominatom-types';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-create-event',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink, LocationSearchBar],
+  imports: [CommonModule, FormsModule, RouterLink, LocationSearchResults, ReactiveFormsModule],
   templateUrl: './create-event.html',
   styleUrl: './create-event.css',
 })
 export class CreateEvent {
+  private formBuilder = inject(FormBuilder);
   private eventService = inject(EventService);
   private router = inject(Router);
 
+  public isLoading = signal<boolean>(false);
+  public isSuccess = signal<boolean>(false);
+  public error = signal<string | null>(null);
+
   public place = signal<NominatimLocation | null>(null);
-  public isSubmitting = signal(false);
 
-  public eventData = {
-    title: '',
-    description: '',
-    dateTime: '',
-    endAt: '',
-  };
+  public eventForm = this.formBuilder.nonNullable.group({
+    title: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(100)]],
+    description: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(300)]],
+    placeDisplayName: [
+      '',
+      [Validators.required, Validators.minLength(1), Validators.maxLength(100)],
+    ],
+    dateTime: ['', [Validators.required]],
+    endAt: ['', [Validators.required]],
+  });
 
-  onSubmit() {
+  public onSubmit() {
+    const { title, description, placeDisplayName, dateTime, endAt } = this.eventForm.value;
     const place = this.place();
-    if (!this.eventData.title || !this.eventData.dateTime || !place) {
+
+    if (this.eventForm.invalid || !title || !description || !placeDisplayName || !dateTime) {
+      return;
+    }
+    if (!place) {
       return;
     }
 
-    this.isSubmitting.set(true);
-
-    const payload: CreateEventRequest = {
-      title: this.eventData.title,
-      description: this.eventData.description,
-      placeDisplayName: place.display_name,
-      placeId: place.place_id,
-      lat: place.lat,
-      lon: place.lon,
-      dateTime: new Date(this.eventData.dateTime).toISOString(),
-      endAt: this.eventData.endAt ? new Date(this.eventData.endAt).toISOString() : undefined,
-    };
-
-    console.log(payload);
-
-    this.eventService.createEvent(payload).subscribe({
-      next: () => this.router.navigate(['/events']),
-      error: (err: any) => {
-        console.error('Submission Error:', err.error);
-        this.isSubmitting.set(false);
-      },
-    });
+    this.isLoading.set(true);
+    firstValueFrom(
+      this.eventService.createEvent({
+        title,
+        description,
+        placeDisplayName,
+        dateTime,
+        endAt,
+        lat: place.lat,
+        lon: place.lon,
+        placeId: place.place_id,
+      })
+    )
+      .then(({ event }) => {
+        this.isSuccess.set(true);
+        this.error.set(null);
+        this.router.navigate(['/event', event.id]);
+      })
+      .catch((error) => {
+        if (error.error.message) {
+          this.error.set(error.error.message);
+          this.isSuccess.set(false);
+          return;
+        }
+        console.error('Error logging in:', error);
+        this.error.set('An unexpected error occurred. Please try again.');
+        this.isSuccess.set(false);
+      })
+      .finally(() => {
+        this.isLoading.set(false);
+      });
   }
 
-  onLocationSelected(location: NominatimLocation) {
+  public onLocationSelected(location: NominatimLocation) {
     this.place.set(location);
   }
 }
