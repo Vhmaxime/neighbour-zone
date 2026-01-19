04c1b4979ab6fe211d549d5b69f987dbe78fc627
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-01-19 15:11:11 +0100

    refactor(edit-event): unify form structure

--- frontend/src/app/pages/events/edit-event/edit-event.ts
+++ frontend/src/app/pages/events/edit-event/edit-event.ts
@@ -1,8 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { firstValueFrom } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, firstValueFrom, switchMap } from 'rxjs';
 import { EventService } from '../../../services/event';
 import { CreateEventRequest, Event } from '../../../types/api.types';
 import { AuthService } from '../../../services/auth';
@@ -10,11 +10,23 @@ import { ActionButton } from '../../../components/action-button/action-button';
 import { BackButton } from '../../../components/back-button/back-button';
 import { LoadingComponent } from '../../../components/loading-component/loading-component';
 import { Title } from '@angular/platform-browser';
+import { NominatimLocation } from '../../../types/nominatom-types';
+import { LocationSearchResults } from '../../../components/location-search-bar/location-search-results';
+import { NominatimService } from '../../../services/nominatim';
 
 @Component({
   selector: 'app-edit-event',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink, ActionButton, BackButton, LoadingComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink,
+    ActionButton,
+    BackButton,
+    LoadingComponent,
+    ReactiveFormsModule,
+    LocationSearchResults,
+  ],
   templateUrl: './edit-event.html',
   styleUrl: './edit-event.html',
 })
@@ -26,9 +38,15 @@ export class EditEvent {
   private authService = inject(AuthService);
   private formBuilder = inject(FormBuilder);
   private titleService = inject(Title);
+  private nominatimService = inject(NominatimService);
 
   // Get ID from route params
   public eventId = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  public user = this.authService.getUser();
+
+  // State signals for location search
+  public searchResults = signal<NominatimLocation[]>([]);
+  public place = signal<NominatimLocation | null>(null);
 
   // State signals
   public isLoading = signal(true);
@@ -39,7 +57,7 @@ export class EditEvent {
   public event = signal<Event | null>(null);
 
   // Form definition
-  public editForm = this.formBuilder.nonNullable.group({
+  public eventForm = this.formBuilder.nonNullable.group({
     title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
     description: ['', [Validators.maxLength(150)]],
     placeDisplayName: [
@@ -54,6 +72,19 @@ export class EditEvent {
     this.isLoading.set(true);
     this.loadEvent();
     this.isLoading.set(false);
+
+    this.eventForm.controls.placeDisplayName.valueChanges
+      .pipe(
+        filter((query) => !!query && query.length >= 3),
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((query) => this.nominatimService.searchLocation(query)),
+      )
+      .subscribe({
+        next: (results) => {
+          this.searchResults.set(results);
+        },
+      });
   }
 
   private loadEvent() {
@@ -62,7 +93,12 @@ export class EditEvent {
 
     firstValueFrom(this.eventService.getEvent(this.eventId))
       .then(({ event }) => {
+        if (this.user?.sub !== event.organizer.id) {
+          this.router.navigate(['/events', this.eventId]);
+          return;
+        }
         this.event.set(event);
+        this.setFormValues(event);
         this.titleService.setTitle(event.title);
       })
       .catch((err) => {
@@ -77,9 +113,80 @@ export class EditEvent {
       });
   }
 
-  onSubmit() {}
+  private setFormValues(event: Event) {
+    const { title, description, placeDisplayName, dateTime, endAt, lat, lon, placeId } = event;
+    this.eventForm.patchValue({
+      title,
+      description,
+      placeDisplayName,
+      dateTime,
+      endAt: endAt ?? undefined,
+    });
 
-  onEventDeleted() {
+    this.place.set({
+      lat,
+      lon,
+      display_name: placeDisplayName,
+      place_id: placeId,
+    });
+  }
+
+  public onSubmit() {
+    this.eventForm.markAllAsTouched();
+
+    const { title, description, placeDisplayName, dateTime, endAt } = this.eventForm.value;
+
+    const place = this.place();
+
+    if (this.eventForm.invalid || !title || !placeDisplayName || !dateTime) {
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.error.set(null);
+    this.isSuccess.set(false);
+
+    if (!place) {
+      this.error.set('Please search for a location and select one from the list.');
+      this.isLoading.set(false);
+      return;
+    }
+
+    firstValueFrom(
+      this.eventService.createEvent({
+        title,
+        description,
+        placeDisplayName,
+        dateTime: new Date(dateTime).toISOString(),
+        endAt: endAt ? new Date(endAt).toISOString() : undefined,
+        lat: place.lat,
+        lon: place.lon,
+        placeId: place.place_id,
+      }),
+    )
+      .then(({ event }) => {
+        this.isSuccess.set(true);
+        this.router.navigate(['/events', event.id]);
+      })
+      .catch((error) => {
+        this.error.set('Failed to create event. Please try again later.');
+        console.error('Submission Error:', error);
+      })
+      .finally(() => {
+        this.isLoading.set(false);
+      });
+  }
+
+  public onEventDeleted() {
     this.router.navigate(['/events']);
+  }
+
+  // Location selection handler
+  public onLocationSelected(location: NominatimLocation) {
+    this.place.set(location);
+    this.searchResults.set([]);
+
+    // emitEvent: false prevents this update from triggering the search again!
+    this.eventForm.patchValue({ placeDisplayName: location.display_name }, { emitEvent: false });
   }
 }
