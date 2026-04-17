9b273521e26d5ac60e6998f33302ec3689fcc701
guiume123 <huysmans_guillaume@hotmail.com>
2026-04-17 19:16:52 +0200

    refactor(messagservice) removed add to local state

--- frontend/src/app/services/messaging.service.ts
+++ frontend/src/app/services/messaging.service.ts
@@ -93,10 +93,6 @@ export class MessagingService {
       }
 
       const message = await response.json();
-      
-      // Add to local state
-      const currentMessages = this.messagesSubject.value;
-      this.messagesSubject.next([...currentMessages, message]);
 
       return message;
     } catch (error) {
